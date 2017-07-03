2b42749fa59f4015fa2cf97215c3feb53d6af616
communityalex <ndrpub@gmail.com>
2017-07-03 15:03:37 +1000

    cleaning ast

--- compiler/ast.c
+++ compiler/ast.c
@@ -1,22 +1,17 @@
 
 #include "ast.h"
 
-#define SET_BASE(node,type_tok,_meta)   node->base.tag = tagv; node->base.token = _tok; node->base.meta = _meta
-#define SET_CONTEXT(ctxt)                     node->base.context = ctxt
-
 #define NEW_NODE(name) struct name* node = malloc(sizeof(struct name))
 #define RETURN_NODE return (struct node*) node
 
-struct node* create_binary_expression_node(struct token op, struct node* left,
+CREATE_NODE_METHOD(binary_expression)(struct token op, struct node* left,
         struct node* right, struct node* context) {
 
 	if (!left || !right) {
         return NULL;
 	}
 
-    NEW_NODE(node_binary_expression);
-
-	SET_BASE(NODE_BINARY_EXPR, left->token, NULL);
+    NEW_NODE(binary_expression);
 
     node->context = context;
 
@@ -27,13 +22,13 @@ struct node* create_binary_expression_node(struct token op, struct node* left,
 	RETURN_NODE;
 }
 
-struct node* create_unary_expression_node(struct token op, struct node* operand){
+CREATE_NODE_METHOD(unary_expression)(struct token op, struct node* operand){
 
     if (!operand){
         return NULL;
     }
 
-    NEW_NODE(node_unary_expression);
+    NEW_NODE(unary_expression);
 
     node->op = op;
     node->operand = operand;
@@ -41,55 +36,12 @@ struct node* create_unary_expression_node(struct token op, struct node* operand)
     RETURN_NODE;
 }
 
-struct node* create_enum_declaration_node(struct token token, const char* identifier,
-        struct table* table, struct node* context){
-
-    NEW_NODE(node_enum_declaration);
-
-    node->context = context;
-    node->identifier = identifier;
-    node->table = table;
-
-    RETURN_NODE;
-}
-
-struct node* create_function_declaration_node(struct token token, const char* identifier,
-        struct node** params, struct node* context){
-
-    NEW_NODE(node_function_declaration);
-
-    SET_CONTEXT(context);
-
-    node->identifier = identifier;
-    node->params = params;
-
-    RETURN_NODE;
-}
-
-struct node* create_variable_declaration_node(struct token token, const char* identifier,
-    struct node** declarations, struct node* context){
-
-    NEW_NODE(node_variable_declaration);
-
-    SET_CONTEXT(context);
-
-    //node->type = type;
-    node->declarations = declarations;
-
-    RETURN_NODE;
-}
-
-struct node* create_struct_declaration_node(struct token token, const char* identifier){
-
-}
-
 CREATE_NODE_METHOD(loop_statement)(struct token tok, struct node* cond, struct node* stmt,
-    struct node* expr, struct node* decl){
+    struct node* expr, struct node* context){
 
         NEW_NODE(node_loop_statement);
 
-        SETBASE(node, NODE_LOOP_STAT, token, NULL);
-        SETDECL(node, decl);
+        node->context = context;
     	node->cond = cond;
     	node->stmt = stmt;
     	node->expr = expr;
@@ -109,7 +61,8 @@ CREATE_NODE_METHOD(class_declaration)(struct token tok, const char* identifier,
         RETURN_NODE;
 }
 
-CREATE_NODE_METHOD(package_declaration)(struct token tok, const char* identifier, struct node** declarations, void* meta, struct node* context){
+CREATE_NODE_METHOD(package_declaration)(struct token tok, const char* identifier,
+    struct node** declarations, void* meta, struct node* context){
 
     NEW_NODE(package_declaration);
     node->identifier = identifier;
@@ -118,7 +71,8 @@ CREATE_NODE_METHOD(package_declaration)(struct token tok, const char* identifier
     RETURN_NODE;
 }
 
-CREATE_NODE_METHOD(enum_declaration)(struct token tok, const char* identifier, struct symtable* table, void* meta, struct node* context){
+CREATE_NODE_METHOD(enum_declaration)(struct token tok, const char* identifier,
+    struct symtable* table, void* meta, struct node* context){
     NEW_NODE(enum_declaration);
     node->context = context;
     node->identifier = identifier;
@@ -130,9 +84,7 @@ CREATE_NODE_METHOD(function_declaration)(struct token tok, const char* identifie
     struct node_compound_statement* block, void* meta, struct node* context) {
 
     NEW_NODE(function_declaration);
-    node->identifier = identifier;
 
-	SETBASE(node, NODE_FUNCTION_DECL, token, meta);
     node->context = context;
 	node->identifier = identifier;
 	node->params = params;
@@ -143,12 +95,12 @@ CREATE_NODE_METHOD(function_declaration)(struct token tok, const char* identifie
 	RETURN_NODE;
 }
 
-CREATE_NODE_METHOD(variable_declation)(gtoken_s token, gtoken_t type, gtoken_t access_specifier, gtoken_t storage_specifier, gnode_r *declarations, void *meta, gnode_t *decl) {
+CREATE_NODE_METHOD(variable_declation)(struct token token, struct token type,
+    struct node** declarations, struct node* context) {
 
     NEW_NODE(variable_declaration);
 
-	SETBASE(node, NODE_VARIABLE_DECL, token, meta);
-    SETDECL(node, decl);
+    node->context = context;
 	node->type = type;
 	node->access = access_specifier;
 	node->storage = storage_specifier;
@@ -157,15 +109,29 @@ CREATE_NODE_METHOD(variable_declation)(gtoken_s token, gtoken_t type, gtoken_t a
 	RETURN_NODE;
 }
 
-CREATE_NODE_METHOD(variable)(struct token tok, const char* identifier, const char* annotation_type, struct node* expr, struct node* context) {
+CREATE_NODE_METHOD(variable)(struct token tok, const char* identifier,
+    struct node* expr, struct node* context) {
 
     NEW_NODE(variable);
 
-	SETBASE(node, NODE_VARIABLE, token, NULL);
     node->context = context;
 	node->identifier = identifier;
 	node->annotation_type = annotation_type;
 	node->expr = expr;
 
 	RETURN_NODE;
+}
+
+CREATE_NODE_METHOD(contract_declaration)(struct token tok, const char* identifier,
+    struct node* super, struct node** interfaces, struct node** declarations, void* meta, struct node* context){
+
+    NEW_NODE(contract_declaration);
+
+    node->context = context;
+    node->identifier = identifier;
+    node->super = super;
+    node->interfaces = interfaces;
+    node->declarations = declarations;
+
+    RETURN_NODE;
 }
