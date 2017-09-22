b60a4938b077af87a067dd93b190171b0e6a0996
communityalex <ndrpub@gmail.com>
2017-09-22 22:22:29 +1000

    adding resolver tests

--- compiler/typing/resolver.go
+++ compiler/typing/resolver.go
@@ -11,10 +11,15 @@ func ResolveExpression(e ast.ExpressionNode) Type {
 type Resolver func(e ast.ExpressionNode) Type
 
 var resolvers = map[ast.NodeType]Resolver{
-	ast.Literal:         resolveLiteralExpression,
-	ast.IndexExpression: resolveIndexExpression,
-	ast.CallExpression:  resolveCallExpression,
-	ast.SliceExpression: resolveSliceExpression,
+	ast.Literal:          resolveLiteralExpression,
+	ast.IndexExpression:  resolveIndexExpression,
+	ast.CallExpression:   resolveCallExpression,
+	ast.SliceExpression:  resolveSliceExpression,
+	ast.MapLiteral:       resolveMapLiteralExpression,
+	ast.ArrayLiteral:     resolveArrayLiteralExpression,
+	ast.BinaryExpression: resolveBinaryExpression,
+	ast.UnaryExpression:  resolveUnaryExpression,
+	ast.Reference:        resolveReference,
 }
 
 func resolveLiteralExpression(e ast.ExpressionNode) Type {
@@ -40,4 +45,24 @@ func resolveSliceExpression(e ast.ExpressionNode) Type {
 func resolveMapLiteralExpression(e ast.ExpressionNode) Type {
 	// must be literal
 	m := e.(ast.MapLiteralNode)
+}
+
+func resolveArrayLiteralExpression(e ast.ExpressionNode) Type {
+	// must be literal
+	m := e.(ast.ArrayLiteralNode)
+}
+
+func resolveBinaryExpression(e ast.ExpressionNode) Type {
+	// must be literal
+	m := e.(ast.MapLiteralNode)
+}
+
+func resolveUnaryExpression(e ast.ExpressionNode) Type {
+	// must be literal
+	m := e.(ast.MapLiteralNode)
+}
+
+func resolveReference(e ast.ExpressionNode) Type {
+	// must be literal
+	m := e.(ast.ReferenceNode)
 }
--- compiler/typing/resolver_test.go
+++ compiler/typing/resolver_test.go
@@ -1,1 +1,38 @@
 package typing
+
+import (
+	"testing"
+
+	"github.com/end-r/goutil"
+	"github.com/end-r/guardian/compiler/ast"
+	"github.com/end-r/guardian/compiler/lexer"
+)
+
+func TestResolveLiteral(t *testing.T) {
+	r := new(ast.LiteralNode)
+	r.LiteralType = lexer.LiteralString
+}
+
+func TestResolveMapLiteral(t *testing.T) {
+	m := new(ast.MapLiteralNode)
+	m.Key = new(ast.ReferenceNode)
+	m.Key.Names = []string{"string"}
+}
+
+func TestResolveArrayLiteral(t *testing.T) {
+	r := new(ast.ArrayLiteralNode)
+	r.LiteralType = lexer.LiteralString
+	goutil.Assert()
+}
+
+func TestResolveBinaryExpression(t *testing.T) {
+	b := new(ast.BinaryExpressionNode)
+	r.LiteralType = lexer.LiteralString
+	goutil.Assert()
+}
+
+func TestResolveUnaryExpression(t *testing.T) {
+	b := new(ast.UnaryExpressionNode)
+	r.LiteralType = lexer.LiteralString
+	goutil.Assert()
+}
