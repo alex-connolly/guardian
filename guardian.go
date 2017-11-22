0fb4f38bd5b6aa04224e5b04977b0affd9016e91
communityalex <ndrpub@gmail.com>
2017-11-22 22:59:29 +1100

    adding util and interfaces

--- guardian.go
+++ guardian.go
@@ -1,17 +1,59 @@
 package guardian
 
 import (
+	"fmt"
+
+	"githbu.come/end-r/guardian/validator"
+
 	"github.com/end-r/guardian/compiler/ast"
 	"github.com/end-r/vmgen"
 )
 
-// A Traverser is the mechanism through which all vm-specific features are applied
+// A VMTranslator is the mechanism through which all vm-specific features are applied
 // to the Guardian AST: bytecode generation, type enforcement
 type VM interface {
 	Traverse(ast.Node) vmgen.Bytecode
 	Builtins() map[string]Builtin
 	Types() map[string]Type
 	Permitted() map[ast.NodeType]bool
+}
+
+func reportErrors(category string, errs []util.Error) {
+	msg := fmt.Sprintf("%s Errors\n", category)
+	for _, e := range errs {
+		msg += fmt.Sprintf("%s", e.msg)
+	}
+	fmt.Println(msg)
+}
+
+func CompileBytes(vm VM, bytes []byte) {
+	tokens, errs := lexer.Lex(bytes)
+
+	if errs != nil {
+		reportErrors("Lexing", errs)
+	}
+
+	ast, errs := parser.Parse(tokens)
+
+	if errs != nil {
+		reportErrors("Parsing", errs)
+	}
+
+	errs := validator.Validate(vm, ast)
+
+	if errs != nil {
+		reportErrors("Type Valdidation", errs)
+	}
+
+	bytecode, errs := vm.Traverse(ast)
+
+	if errs != nil {
+		reportErrors("Bytecode Generation", errs)
+	}
+}
+
+func CompileFile(vm VM path string){
+
 }
 
 /*
